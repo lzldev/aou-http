362c2c929c0033b0ceda9875696aeb159b6af342
Lucas Costa <francisco.lucas0507@gmail.com>
2024-05-02 01:01:13 -0300

    server binary

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use anyhow::anyhow;
+use std::net::SocketAddr;
+use std::time::Duration;
+
+mod request;
+
+use request::Request;
+use tokio::time::Instant;
+use tokio::{
+  io::{AsyncReadExt, AsyncWriteExt},
+  net::{TcpListener, TcpStream},
+};
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+  let port = "127.0.0.1:7070".parse::<SocketAddr>().unwrap();
+  let socket = TcpListener::bind(port).await.expect("Couldn't bind server");
+
+  println!("Server Listening in : {port}");
+
+  loop {
+    let con = socket.accept().await.expect("Socket failed to accept");
+
+    tokio::spawn(async move {
+      let n = process_request(con).await;
+
+      if let Err(e) = n {
+        dbg!(e);
+      }
+
+      ()
+    });
+  }
+}
+
+async fn process_request(socket: (TcpStream, SocketAddr)) -> Result<(), anyhow::Error> {
+  let (mut stream, _) = socket;
+
+  let mut buf = Vec::new();
+
+  let mut _req: Option<Request> = {
+    //TODO:Remove into Parse_frame
+    loop {
+      let buf_len = buf.len();
+      let n = tokio::select! {
+        n = stream.read_buf(&mut buf) => {
+          if let Err(n) = n {
+            return Err(n.into());
+          }
+
+          n.unwrap()
+        },
+        _ = tokio::time::sleep(Duration::from_millis(10)) => return Err(anyhow!("Timedout"))
+      };
+
+      if buf_len == 0 && n == 0 {
+        break None;
+      }
+      if buf_len > 0 {
+        if let Ok(req) = Request::parse_request(&mut buf) {
+          break Some(req);
+        }
+
+        if n == 0 {
+          break None;
+        }
+      }
+    }
+  };
+
+  let _req = _req.ok_or(anyhow!("Can't unwrap _req data"))?;
+
+  let body_buf = format!("\nHello World\n{:#?}", Instant::now());
+  let body_length = body_buf.len();
+
+  let response = format!(
+    "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length:{}\r\n\r\n{}\r\n\r\n",
+    body_length, body_buf
+  );
+
+  let _ = stream.write_all(response.as_bytes()).await?;
+
+  stream.flush().await?;
+
+  Ok(())
+}
--- src/request.rs
+++ src/request.rs
@@ -1,118 +1,136 @@
+use anyhow::{anyhow, Context};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Request {}
-
-impl Request {
-  //   fn parse_request(buf: [u8]) -> Result<Request, anyhow::Error> {
-  //     todo!()
-  //   }
+pub struct Request<'req> {
+  pub buf: &'req [u8],
+  pub head: RequestHead<'req>,
+  pub headers: RequestHeaders<'req>,
+  pub body: &'req [u8],
 }
 
-impl Request {
-  pub fn parse_request_with_vec(buf: &[u8]) {
-    let mut lines = buf.split(|b| b == &b'\n');
+type RequestHeaders<'req> = Vec<(&'req [u8], &'req [u8])>; // Probably should be a hashmap already?
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RequestHead<'req> {
+  pub method: &'req [u8],
+  pub path: &'req [u8],
+  pub http_version: &'req [u8],
+}
 
-    let head = lines.next().unwrap();
+impl<'req> Request<'req> {
+  // pub fn parse_request_with_vec(buf: &[u8]) -> Request {
+  //   let mut offset: usize = 0;
+  //   let mut lines = buf.split(|b| b == &b'\n');
 
-    let mut head_split = head.split(|b| b == &b' ');
+  //   let head = lines.next().context("")?;
+  //   offset = offset + head.len() + 1;
 
-    let (method, path, http_version) = (
-      head_split.next().unwrap(),
-      head_split.next().unwrap(),
-      head_split.next().unwrap(),
-    );
+  //   let mut head_split = head.split(|b| b == &b' ');
 
-    assert_eq!(method, b"GET");
-    assert_eq!(path, b"/");
-    assert_eq!(http_version, b"HTTP/1.1");
+  //   let (method, path, http_version) = (
+  //     head_split.next().unwrap(),
+  //     head_split.next().unwrap(),
+  //     head_split.next().unwrap(),
+  //   );
 
-    let mut header_vec: Vec<(&[u8], &[u8])> = Vec::new();
+  //   let mut headers: RequestHeaders = Vec::new();
 
-    for line in lines.by_ref() {
-      if line == b"" {
-        break;
-      }
-      let mut split = line.split(|b| b == &b':');
+  //   for line in lines.by_ref() {
+  //     offset = line.len() + 1;
+  //     if line == b"" {
+  //       break;
+  //     }
 
-      let (header, value) = (split.next().unwrap(), split.next().unwrap());
+  //     let mut split = line.split(|b| b == &b':');
 
-      //TODO:REMOVE DEBUG
-      let hh = String::from_utf8_lossy(header);
-      let hv = String::from_utf8_lossy(value);
+  //     let (header, value) = (split.next().unwrap(), split.next().unwrap());
+  //     headers.push((header, value));
+  //   }
 
-      // eprintln!("iter -> k/v | {hh}|{hv}");
+  //   assert!(offset <= buf.len(), "body offset bigger than buf length");
+  //   let body = &buf[..offset];
 
-      header_vec.push((header, value));
-    }
+  //   let head = RequestHead {
+  //     method,
+  //     path,
+  //     http_version,
+  //   };
 
-    assert_eq!(header_vec.len(), 2);
+  //   Request {
+  //     buf,
+  //     head,
+  //     headers,
+  //     body,
+  //   }
+  // }
 
-    let lines = lines.skip(header_vec.len());
-
-    let hint = lines.size_hint();
-    let col = lines.collect::<Vec<_>>();
-    // eprintln!("HINT: {hint:?}");
-    // eprintln!("col: {col:?}");
-  }
-
-  pub fn parse_request(buf: &[u8]) -> (&[u8], &[u8]) {
+  pub fn parse_request(buf: &mut [u8]) -> Result<Request, anyhow::Error> {
     let mut offset: usize = 0;
     let mut lines = buf.split(|b| b == &b'\n');
 
-    let head = lines.next().unwrap();
-    offset = offset.wrapping_add(head.len() + 1); // Add Headsize + \n to offset
+    let (method, path, http_version) = {
+      let head = lines.next().context("Head not found?")?;
+      offset = offset.wrapping_add(head.len() + 1); // Add size of Head + \n to offset
+      let mut head_split = head.split(|b| b == &b' ');
 
-    let mut head_split = head.split(|b| b == &b' ');
+      let method = head_split.next().ok_or(anyhow!("Method not found"))?;
+      let path = head_split.next().ok_or(anyhow!("Path not found Path"))?;
+      let http_version = head_split.next().ok_or(anyhow!("Http Version not found"))?;
 
-    let (method, path, http_version) = (
-      head_split.next().unwrap(),
-      head_split.next().unwrap(),
-      head_split.next().unwrap(),
-    );
+      assert_eq!(method[0], buf[0], "Method[0] and buf[0] not equal");
+      assert_eq!(method[0], buf[0], "Method[0] and buf[0] not equal");
 
-    assert_eq!(method, b"GET");
-    assert_eq!(path, b"/");
-    assert_eq!(http_version, b"HTTP/1.1");
+      Ok::<_, anyhow::Error>((method, path, http_version))
+    }?;
+
+    assert_eq!(http_version, b"HTTP/1.1\r");
 
     let headers = lines
-      .clone()
-      .take_while(|b| *b != b"")
-      .map(|header| {
+      .take_while(|b| *b != b"" && *b != b"\r") //TODO: Handle the \r correctly
+      .filter_map(|header| {
         offset = offset.wrapping_add(header.len() + 1); // Add line size + \n to offset
 
         let mut split = header.split(|b| b == &b':');
 
-        let (header, value) = (split.next().unwrap(), split.next().unwrap());
+        let (header, value) = (split.next()?, split.next()?);
 
-        //TODO:REMOVE DEBUG
-        let hh = String::from_utf8_lossy(header);
-        let hv = String::from_utf8_lossy(value);
-
-        // eprintln!("iter -> k/v | {hh}|{hv}");
-
-        (header, value)
+        Some((header, value))
       })
       .collect::<Vec<_>>();
 
-    let lines = lines.skip(headers.len() + 1);
-    offset = offset.wrapping_add(1); // Skip the empty line + \n
+    let buf_len = buf.len();
+    // if offset < buf_len - 1 {
+    // Check if the stream ends here
+    //   offset = offset.wrapping_add(1)
+    // }; // Skip the empty line between Headers and body + \n
 
-    let len = buf.len();
+    if offset >= buf_len {
+      return Err(anyhow!("Offset Larger than buffer size."));
+    }
+
     assert!(
-      offset <= len,
-      "Offset larger than buffer size : Offset {offset} : Buf {len}"
+      offset <= buf_len,
+      "Buf:{:#?}\nOffset larger than buffer size : Offset {offset} : Buf {buf_len} Headers:{}",
+      String::from_utf8_lossy(buf),
+      headers.len()
     );
 
-    let hint = lines.size_hint();
-    let col = lines.collect::<Vec<_>>();
     let body = &buf[offset..];
 
-    assert_eq!(body.len(), 0, "body lenght isn't 0");
+    let head = RequestHead {
+      method,
+      path,
+      http_version,
+    };
 
-    // eprintln!("HINT: {hint:?}");
-    // eprintln!("col: {col:?}");
-    (buf, body)
+    let req = Request {
+      buf,
+      head,
+      headers,
+      body,
+    };
+
+    Ok(req)
   }
 }
 
@@ -130,25 +148,93 @@ Accept-Language: en
 
   #[tokio::test]
   async fn test_parse_request() {
-    let req = Request::parse_request(GET_REQUEST_MOCK);
+    let req = Request::parse_request(GET_REQUEST_MOCK).unwrap();
 
-    eprintln!("{req:?}");
+    let size_buf = std::mem::size_of_val(GET_REQUEST_MOCK);
+    let size_u8 = std::mem::size_of_val(&255u8);
+    let size = std::mem::size_of_val(&req);
+    let size_req_buf = std::mem::size_of_val(req.buf);
+    let size_headers = std::mem::size_of_val(&req.headers);
+    let size_head = std::mem::size_of_val(&req.head);
+    let size_body = std::mem::size_of_val(req.body);
+
+    eprintln!("Request: {req:?}");
+    eprintln!("Sizes ---");
+    eprintln!("U8: {size_u8:?}");
+    eprintln!("Original: {size_buf:#?}");
+    eprintln!("Request: {size:#?}");
+    eprintln!("Internal---");
+    eprintln!("  Raw: {size_req_buf:#?}");
+    eprintln!("  Head: {size_head:#?}");
+    eprintln!("  Headers: {size_headers:#?}");
+    eprintln!("  Body: {size_body:#?}");
+    eprintln!(
+      "    TOTAL: {:#?}",
+      size_req_buf + size_head + size_headers + size_body
+    )
+  }
+
+  const POST_REQUEST_MOCK: &[u8] = b"POST /create HTTP/1.1
+Host: www.example.com
+Accept-Language: en
+Content-Type: application/json
+
+{
+  \"hello\":\"world\"
+}";
+  #[tokio::test]
+  async fn test_parse_request_with_body() {
+    let req = Request::parse_request(&mut POST_REQUEST_MOCK).unwrap();
+
+    let size_buf = std::mem::size_of_val(POST_REQUEST_MOCK);
+    let size_u8 = std::mem::size_of_val(&255u8);
+    let size = std::mem::size_of_val(&req);
+    let size_req_buf = std::mem::size_of_val(req.buf);
+    let size_headers = std::mem::size_of_val(&req.headers);
+    let size_head = std::mem::size_of_val(&req.head);
+    let size_body = std::mem::size_of_val(req.body);
+
+    eprintln!("Request: {req:?}");
+    eprintln!("Sizes ---");
+    eprintln!("U8: {size_u8:?}");
+    eprintln!("Original: {size_buf:#?}");
+    eprintln!("Request: {size:#?}");
+    eprintln!("Internal---");
+    eprintln!("  Raw: {size_req_buf:#?}");
+    eprintln!("  Head: {size_head:#?}");
+    eprintln!("  Headers: {size_headers:#?}");
+    eprintln!("  Body: {size_body:#?}");
+    eprintln!("    TOTAL: {:#?}", size_head + size_headers + size_body);
+
+    assert_eq!(
+      req.head.method, b"POST",
+      "Request method doesn't match MOCK Method"
+    );
+    assert_eq!(
+      size_body,
+      std::mem::size_of_val(
+        b"{
+  \"hello\":\"world\"
+}"
+      ),
+      "Body bigger than actual json"
+    );
   }
 
   #[tokio::test]
   async fn bench_parse_request() {
-    let size = 1_000_000;
+    let size = 20_000_000;
 
     let start = Instant::now();
     for _ in 0..size {
-      Request::parse_request(GET_REQUEST_MOCK);
+      let _ = Request::parse_request(&mut POST_REQUEST_MOCK);
     }
     eprintln!("ITER TOOK: {:?}", start.elapsed());
 
     let start = Instant::now();
-    for _ in 0..size {
-      Request::parse_request_with_vec(GET_REQUEST_MOCK);
-    }
+    // for _ in 0..size {
+    //   let _ = Request::parse_request_with_vec(POST_REQUEST_MOCK);
+    // }
     eprintln!("WITH VEC TOOK: {:?}", start.elapsed());
   }
 }
