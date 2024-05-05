c18d194881938e8fc9d768c9983e090989990377
Lucas Costa <francisco.lucas0507@gmail.com>
2024-05-05 01:49:42 -0300

    parser unit tests

--- src/request/parser.rs
+++ src/request/parser.rs
@@ -120,3 +120,42 @@ impl RequestParser {
     Ok(RequestParseResponse::Success(req))
   }
 }
+
+#[cfg(test)]
+mod unit_tests {
+  use crate::request::{ParserState, RequestParseResponse, RequestParser};
+  #[tokio::test]
+  async fn invalid_http_version() {
+    let buf =
+      b"GET / HTTP/1.0\r\nHost: localhost:3000\r\nThe empty line before the body is missing";
+
+    let parse = RequestParser::parse_request(buf.into(), ParserState::Start { read_until: None });
+
+    assert!(
+      parse.is_err(),
+      "Parse should return a Invalid HTTP Version Error Result"
+    );
+  }
+
+  #[tokio::test]
+  async fn invalid_header_termination() {
+    let buf =
+      b"GET / HTTP/1.1\r\nHost: localhost:3000\r\nThe empty line before the body is missing";
+
+    let parse = RequestParser::parse_request(buf.into(), ParserState::Start { read_until: None });
+
+    assert!(
+      parse.is_ok(),
+      "Parse should not error since it's not sure it's the end of the headers."
+    ); //Check the spec for spaces in header names
+
+    let parse = parse.unwrap();
+
+    let is_incomplete = match parse {
+      RequestParseResponse::Incomplete(_) => true,
+      RequestParseResponse::Success(_) => false,
+    };
+
+    assert!(is_incomplete, "Parser should return a Incomplete result");
+  }
+}
